e4126a75a0b079ae73ab59a97fbc4e6869801f0e
justinnjamesleong <justinnjamesleong@gmail.com>
2022-10-28 10:59:43 +0800

    Commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// Import packages
+const { response } = require('express')
+const express = require('express')
+const morgan = require('morgan')
+
+//Configure out port
+//Check if it is set in the environment variable, if not we default to 3000
+const port = process.env.PORT || 3000
+
+//Create an instance of express application
+const app = express()
+
+//Log all request
+app.use(morgan('common'))
+
+// Get /
+app.get('/',(req, resp) => {
+    // what is the media type
+    resp.type('text/html')
+    // set the status code
+    resp.status(200)
+    //send the response
+    resp.send(`<h1>The time is now ${new Date()}</h1>`)
+})
+
+//Start our web application
+app.listen(port, () => {
+    console.info(`Starting application on port ${port} at ${new Date()}`)
+})
